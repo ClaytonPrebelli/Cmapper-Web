be3e1b75e8189a9a44a3bf711935d31892882ae8
Clayton Prebelli <clayton.pires@aluno.unip.br>
2020-12-31 02:52:14 -0300

    corrigidas extensoes

--- valida.php
+++ valida.php
@@ -5,7 +5,7 @@
     on usuarios.permissao = permissoes.idpermission
     where usuarios.usuario = '$login_temp' and permissoes.permission_descricao = 'administrador';") or die("erro ao selecionar");
     if (mysql_num_rows($verifica)<=0){
-        header("Location: erro_perm.php"); 
+        header("Location: erro_perm.html"); 
         die();}else{
             header("Location:cp.php");
         }
