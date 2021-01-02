b1018d43c4d469e330f63c6b89f6a862c27c673f
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-01-01 21:03:36 -0300

    utilizado bd umbler

--- conectar.php
+++ conectar.php
@@ -1,4 +1,4 @@
 <?php
-$connect = mysql_connect('mysql742.umbler.com','cmapper','Mistic.1708')or die("Impossivel conectar");
+$connect = mysql_connect('mysql742.umbler.com:41890','cmapper','Mistic.1708')or die("Impossivel conectar");
                     $db = mysql_select_db('cmapper');
                     ?>
